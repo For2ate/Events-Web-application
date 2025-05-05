b9e7b5affe5aad9940222385884c17570abb3d8a
Ivan_Skrypinski <ivanskrypinski@gmail.com>
2025-05-05 23:38:44 +0300

    dto

---
+++ EventApp.Event.Api/EventApp.Models/EventRegistrationDTO/Request/RegisterUserToEventRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EventApp.Models.EventRegistrationDTO.Request {
+
+    public class RegisterUserToEventRequestModel {
+
+        public Guid UserId { get; set; }
+
+        public Guid EventId { get; set; }
+
+    }
+
+}
